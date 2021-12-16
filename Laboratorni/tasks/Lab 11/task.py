a14165e1a8ccfb0f92a04d2ace7ad1a48df68205
SarvadiDarina <56667667+SarvadiDarina@users.noreply.github.com>
2021-12-16 12:46:46 +0200

    11

--- Laboratorni/tasks/Lab 11/task.py
+++ Laboratorni/tasks/Lab 11/task.py
@@ -12,9 +12,13 @@
 '''
 
 class TRectangle:
-    def __init__(self, a=0, b=0):
-        self.a = a
-        self.b = b
+    def __init__(self, a=1, b=1):
+        if a >= 1:
+            self.a = a
+        elif b >= 1:
+            self.b = b
+        else:
+            raise Exception('Can\'t be less than 1')
     def input(self):
         self.a = int(input('I сторона = '))
         self.b = int(input('II сторона = '))
@@ -23,10 +27,23 @@ class TRectangle:
     def get_p(self):
         return 2 * (self.a + self.b)
     def __eq__(self, r2):
-        return TRectangle < r2
+        return TRectangle == r2(self.a, self.b)
+    def __lt__(self, r2):
+        return TRectangle < r2(self.a, self.b)
+    def __le__(self, r2):
+        return TRectangle <= r2(self.a, self.b)
+    def __ne__(self, r2):
+        return TRectangle != r2(self.a, self.b)
+    def __gt__(self, r2):
+        return TRectangle > r2(self.a, self.b)
+    def __ge__(self, r2):
+        return TRectangle >= r2(self.a, self.b)
     def __add__(self, other):
         return TRectangle(self.a + other, self.b + other)
     def __sub__(self, other):
         return TRectangle(self.a - other, self.b - other)
     def __mul__(self, x):
         return TRectangle(self.a*x, self.b*x)
+
+tr1 = TRectangle(4, 7)
+tr2 = TRectangle(2, 5)
